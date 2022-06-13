53d6680dc3499d3828e63f8f33d53368f65804a6
Yuuna <comaAberration@gmail.com>
2022-06-13 21:24:43 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,55 +1,45 @@
 import { io } from 'socket.io-client';
 
-const profile = document.getElementById('user-id')
-const form = document.getElementById('form')
-const messageInput = document.getElementById('message-input')
+const profile = document.getElementById('profile')
+const msg_input = document.getElementById('msg-input')
 
 const ip = 'xxx.xxx.xxx.xxx'
-
 const socket = io(`http://${ip}:3000`)
 
 socket.on('connect', () => {
-  display_user_profile(socket.id)
+  display_user_id(socket.id)
   socket.emit('online')
 })
 
-socket.on('get-msg', (msg, id) => {
-  display_other_msg(`${id}: ${msg}`)
+socket.on('get-msg', (msg, user_id) => {
+  display_msg(`${user_id}: ${msg}`)
 })
 
 socket.on('get-online-list', (online_list) => {
   display_online_list(`Online: ${online_list}`)
 })
 
-form.addEventListener('submit', (e) => {
+document.getElementById('form').addEventListener('submit', (e) => {
   e.preventDefault()
-  const msg = messageInput.value
-  if (msg === '')
-    return
+  const msg = msg_input.value
+  if (msg === '') return
   let id = socket.id.substring(0, 6)
-  display_self_msg(`${id}: ${msg}`)
+  display_msg(`${id}: ${msg}`)
   socket.emit('send-msg', msg, id)
-  messageInput.value = ''
+  msg_input.value = ''
 })
 
-function display_user_profile(id) {
+function display_user_id(id) {
   let user_id = id.substring(0, 6)
-  profile.textContent = `Your ID -> [${user_id}]`
+  profile.textContent = `Your ID -> [ ${user_id} ]`
 }
 
 function display_online_list(online_list) {
   document.getElementById('online-list').textContent = online_list
 }
 
-function display_other_msg(msg) {
-  const div = document.createElement('li')
-  div.textContent = msg
-  document.getElementById('message-container').append(div)
-}
-
-function display_self_msg(msg) {
-  const div = document.createElement('li')
-  div.textContent = msg
-  div.style.cssText = 'list-style: square;'
-  document.getElementById('message-container').append(div)
+function display_msg(msg) {
+  const li = document.createElement('li')
+  li.textContent = msg
+  document.getElementById('msg-list').append(li)
 }
